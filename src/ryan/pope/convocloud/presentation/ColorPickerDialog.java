b18dfb110539d947695048f6d64ddb4d53208fa0
Ryan Pope <ryan@ryanpope.com>
2014-08-12 19:32:42 -0500

    Added info icon

--- src/ryan/pope/convocloud/presentation/ColorPickerDialog.java
+++ src/ryan/pope/convocloud/presentation/ColorPickerDialog.java
@@ -1,6 +1,5 @@
 package ryan.pope.convocloud.presentation;
 
-
 import ryan.pope.convocloud.business.ColorPicker;
 import android.app.AlertDialog;
 import android.content.Context;
@@ -8,48 +7,61 @@ import android.content.DialogInterface;
 import android.widget.RelativeLayout;
 import android.widget.RelativeLayout.LayoutParams;
 
-public class ColorPickerDialog extends AlertDialog {
+public class ColorPickerDialog extends AlertDialog
+{
 
-    private ColorPicker colorPickerView;
-    private final OnColorSelectedListener onColorSelectedListener;
+	public interface OnColorSelectedListener
+	{
+		public void onColorSelected(int color);
+	}
 
-    public ColorPickerDialog(Context context, int initialColor, OnColorSelectedListener onColorSelectedListener) {
-        super(context);
+	private ColorPicker colorPickerView;
 
-        this.onColorSelectedListener = onColorSelectedListener;
+	private final OnColorSelectedListener onColorSelectedListener;
 
-        RelativeLayout relativeLayout = new RelativeLayout(context);
-        LayoutParams layoutParams = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
-        layoutParams.addRule(RelativeLayout.CENTER_IN_PARENT);
+	private OnClickListener onClickListener = new DialogInterface.OnClickListener()
+	{
+		@Override
+		public void onClick(DialogInterface dialog, int which)
+		{
+			switch (which)
+			{
+				case BUTTON_POSITIVE:
+					int selectedColor = colorPickerView.getColor();
+					onColorSelectedListener.onColorSelected(selectedColor);
+					break;
+				case BUTTON_NEGATIVE:
+					dialog.dismiss();
+					break;
+			}
+		}
+	};
 
-        colorPickerView = new ColorPicker(context);
-        colorPickerView.setColor(initialColor);
+	public ColorPickerDialog(Context context, int initialColor,
+			OnColorSelectedListener onColorSelectedListener)
+	{
+		super(context);
 
-        relativeLayout.addView(colorPickerView, layoutParams);
+		this.onColorSelectedListener = onColorSelectedListener;
 
-        setButton(BUTTON_POSITIVE, context.getString(android.R.string.ok), onClickListener);
-        setButton(BUTTON_NEGATIVE, context.getString(android.R.string.cancel), onClickListener);
+		RelativeLayout relativeLayout = new RelativeLayout(context);
+		LayoutParams layoutParams = new LayoutParams(
+				android.view.ViewGroup.LayoutParams.MATCH_PARENT,
+				android.view.ViewGroup.LayoutParams.MATCH_PARENT);
+		layoutParams.addRule(RelativeLayout.CENTER_IN_PARENT);
 
-        setView(relativeLayout);
+		colorPickerView = new ColorPicker(context);
+		colorPickerView.setColor(initialColor);
 
-    }
+		relativeLayout.addView(colorPickerView, layoutParams);
 
-    private OnClickListener onClickListener = new DialogInterface.OnClickListener() {
-        public void onClick(DialogInterface dialog, int which) {
-            switch (which) {
-            case BUTTON_POSITIVE:
-                int selectedColor = colorPickerView.getColor();
-                onColorSelectedListener.onColorSelected(selectedColor);
-                break;
-            case BUTTON_NEGATIVE:
-                dialog.dismiss();
-                break;
-            }
-        }
-    };
+		setButton(BUTTON_POSITIVE, context.getString(android.R.string.ok),
+				onClickListener);
+		setButton(BUTTON_NEGATIVE, context.getString(android.R.string.cancel),
+				onClickListener);
 
-    public interface OnColorSelectedListener {
-        public void onColorSelected(int color);
-    }
+		setView(relativeLayout);
+
+	}
 
 }
